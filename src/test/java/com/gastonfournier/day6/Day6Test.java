8cc150d6060bcdbaaad6d0ffd42b1cd7087c6bb3
Gastón Fournier <gastonfournier@gmail.com>
2022-12-17 20:35:39 +0100

    Recover and fix day 6 tests

--- src/test/java/com/gastonfournier/day6/Day6Test.java
+++ src/test/java/com/gastonfournier/day6/Day6Test.java
@@ -4,10 +4,7 @@ import com.gastonfournier.DailyChallengeTest;
 import com.gastonfournier.utils.Solution;
 import org.assertj.core.util.Lists;
 
-import java.util.Arrays;
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class Day6Test extends DailyChallengeTest {
 
@@ -18,39 +15,22 @@ class Day6Test extends DailyChallengeTest {
 
     @Override
     public List<String> getInput() {
-        return Arrays.stream("""
-                $ cd /
-                $ ls
-                dir a
-                14848514 b.txt
-                8504156 c.dat
-                dir d
-                $ cd a
-                $ ls
-                dir e
-                29116 f
-                2557 g
-                62596 h.lst
-                $ cd e
-                $ ls
-                584 i
-                $ cd ..
-                $ cd ..
-                $ cd d
-                $ ls
-                4060174 j
-                8033020 d.log
-                5626152 d.ext
-                7214296 k""".split("\n")).toList();
+        return Lists.list(
+                "mjqjpqmgbljsphdztnvjfqwrcgsmlb",
+                "bvwbjplbgvbhsrlpgdmjqwftvncz",
+                "nppdvjthqldpwncqszvftbrmjlhg",
+                "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg",
+                "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"
+        );
     }
 
     @Override
-    public Integer expected1() {
-        return 95437;
+    public List<Integer> expected1() {
+        return Lists.list(7, 5, 6, 10, 11);
     }
 
     @Override
-    public Integer expected2() {
-        return 1;
+    public List<Integer> expected2() {
+        return Lists.list(19, 23, 23, 29, 26);
     }
 }
